16720b6a5061b1d8b3caa3087cc4a7468e244e40
A****** <bucmeister@icloud.com>
2020-02-17 15:22:21 -0600

    React Router

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from "./DishdetailComponent";
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
   
@@ -16,17 +18,27 @@ class Main extends Component {
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
+  // onDishSelect(dishId) {
+  //   this.setState({ selectedDish: dishId });
+  // }
 
   render() {
+    const HomePage = () => {
+      return(
+        <Home />
+      );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <Dishdetail dish={this.state.selectedDish} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+          <Redirect to="/home" />
+        </Switch>
+        {/* <Menu dishes={this.state.dishes}
+            onClick={(dishId) => this.onDishSelect(dishId)}/> */}
+        {/* <Dishdetail dish={this.state.selectedDish} /> */}
         <Footer />
       </div>
     );
